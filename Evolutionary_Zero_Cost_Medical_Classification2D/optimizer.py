669d736bcde35e30c35aa8d16823ec3ac6fd561a
Muhammad Junaid Ali <junaid199f@gmail.com>
2025-08-06 17:10:14 +0200

    Update optimizer.py

--- Evolutionary_Zero_Cost_Medical_Classification2D/optimizer.py
+++ Evolutionary_Zero_Cost_Medical_Classification2D/optimizer.py
@@ -1,71 +1,125 @@
 import os
+import pickle
 import augment
 import augmentations
 from dataset import Dataset
 from evaluate import Evaluate
 import genotype
 import operations
-from operations_mapping import operations_mapping,attentions
+from operations_mapping import operations_mapping, attentions
 from population import Population
 import utils
-import pickle
 import Surrogate
+
 class Optimizer:
-  def __init__(self, population_size, number_of_generations, crossover_prob, mutation_prob, blocks_size, num_classes,
-               in_channels, epochs, batch_size, layers, n_channels, dropout_rate, retrain,resume_train,cutout,multigpu_num,medmnist_dataset,is_medmnist,check_power_consumption,evaluation_type):
-    self.resume_train = resume_train
-    self.layers = layers
-    if self.resume_train==True:
-      z = open('checkpoints/checkpoints.pkl','rb')
-      #self.pop =  Population(blocks_size, population_size)
-      self.pop = pickle.load(z)
-    else:
-      self.pop = Population(blocks_size, population_size,self.layers)
-    self.population_size = population_size
-    self.number_of_generations = number_of_generations
-    self.crossover_prob = crossover_prob
-    self.is_medmnist = is_medmnist
-    self.check_power_consumption = check_power_consumption
-    self.epochs = epochs
-    self.evaluation_type = evaluation_type
-    self.multigpu_num = multigpu_num
-    self.gpu_devices = ','.join([str(id) for id in range(0,self.multigpu_num)])
-    os.environ["CUDA_VISIBLE_DEVICES"] = self.gpu_devices
-    self.surrogate = Surrogate.Surrogate()
-    self.cutout = cutout
-    self.mutation_prob = mutation_prob
-    self.batch_size = batch_size
-    self.blocks_size = blocks_size
-    self.num_classes = num_classes
-    self.in_channels = in_channels
-    self.medmnist_dataset = medmnist_dataset
-    self.dropout_rate = dropout_rate
-    self.n_channels = n_channels
-    self.intermediate_channels = [2 ** (i + 1) for i in range(1, blocks_size + 1, 1)]
-    self.retrain = retrain
-    self.fitness = []
-    self.networks = []
-    self.trained_individuals = False
-    self.decoded_individuals = []
-    self.offsprings_population = []
-    self.offsprings_fitness = []
-    self.evaluator = Evaluate(self.batch_size,self.medmnist_dataset,self.is_medmnist,self.check_power_consumption)
-    self.attentions = [i for i in range(0, len(attentions))]
-    self.save = 'EXP'
+    """
+    A class to perform optimization using evolutionary algorithms for neural networks.
+    This includes operations such as mutation, crossover, fitness evaluation, and population management.
+    """
 
-    def evolve():
-      pass
+    def __init__(self, population_size, number_of_generations, crossover_prob, mutation_prob, blocks_size,
+                 num_classes, in_channels, epochs, batch_size, layers, n_channels, dropout_rate, retrain,
+                 resume_train, cutout, multigpu_num, medmnist_dataset, is_medmnist, check_power_consumption,
+                 evaluation_type):
+        """
+        Initialize the Optimizer with the given parameters.
+        Arguments:
+        - population_size: Size of the population
+        - number_of_generations: Number of generations to evolve
+        - crossover_prob: Crossover probability
+        - mutation_prob: Mutation probability
+        - blocks_size: Size of the blocks in the model architecture
+        - num_classes: Number of output classes
+        - in_channels: Number of input channels
+        - epochs: Number of epochs for training
+        - batch_size: Batch size for training
+        - layers: Number of layers in the neural network
+        - n_channels: Number of channels in the network
+        - dropout_rate: Dropout rate to avoid overfitting
+        - retrain: Flag to indicate if retraining is required
+        - resume_train: Flag to resume training from a checkpoint
+        - cutout: Whether to apply cutout augmentation
+        - multigpu_num: Number of GPUs to use for training
+        - medmnist_dataset: Whether the dataset is MedMNIST
+        - is_medmnist: Whether the dataset is MedMNIST
+        - check_power_consumption: Flag to monitor power consumption
+        - evaluation_type: Type of evaluation to perform (e.g., accuracy, efficiency)
+        """
+        self.resume_train = resume_train
+        self.layers = layers
+        self.population_size = population_size
+        self.number_of_generations = number_of_generations
+        self.crossover_prob = crossover_prob
+        self.mutation_prob = mutation_prob
+        self.batch_size = batch_size
+        self.blocks_size = blocks_size
+        self.num_classes = num_classes
+        self.in_channels = in_channels
+        self.epochs = epochs
+        self.n_channels = n_channels
+        self.dropout_rate = dropout_rate
+        self.medmnist_dataset = medmnist_dataset
+        self.is_medmnist = is_medmnist
+        self.check_power_consumption = check_power_consumption
+        self.evaluation_type = evaluation_type
+        self.cutout = cutout
+        self.retrain = retrain
+        self.multigpu_num = multigpu_num
+        self.gpu_devices = ','.join([str(id) for id in range(0, self.multigpu_num)])
+        os.environ["CUDA_VISIBLE_DEVICES"] = self.gpu_devices
 
-  def encode(self):
-    return None
+        self.surrogate = Surrogate.Surrogate()
+        self.intermediate_channels = [2 ** (i + 1) for i in range(1, blocks_size + 1)]
+        self.fitness = []
+        self.networks = []
+        self.trained_individuals = False
+        self.decoded_individuals = []
+        self.offsprings_population = []
+        self.offsprings_fitness = []
+        self.evaluator = Evaluate(self.batch_size, self.medmnist_dataset, self.is_medmnist, self.check_power_consumption)
+        self.attentions = [i for i in range(0, len(attentions))]
+        self.save = 'EXP'
 
-  def decode(self):
-    self.decoded_individuals = self.pop.decode_individuals(self.pop.Individuals)
-    # for i in range(self.population_size):
-    # model = Net(pop.individual[0],self.in_channels,self.intermediate_channels,self.num_classes,self.block_size)
+        # Load the population from checkpoint if resuming training
+        if self.resume_train:
+            self._load_population()
 
-  def optimize(self):
-    self.evolve()
+    def _load_population(self):
+        """
+        Load the population from the checkpoint file if resuming training.
+        """
+        try:
+            with open('checkpoints/checkpoints.pkl', 'rb') as z:
+                self.pop = pickle.load(z)
+        except FileNotFoundError:
+            print("Checkpoint file not found, starting with a new population.")
+            self.pop = Population(self.blocks_size, self.population_size, self.layers)
 
+    def evolve(self):
+        """
+        Evolve the population using genetic algorithms (not implemented in this version).
+        This method should handle the core evolutionary operations like mutation, crossover, and fitness evaluation.
+        """
+        pass  # This method is a placeholder and should be implemented.
 
+    def encode(self):
+        """
+        Encodes the population or individuals. This method is a placeholder and should be implemented.
+        """
+        return None  # Placeholder method.
+
+    def decode(self):
+        """
+        Decode the individuals in the population. This method will decode the individuals from their genetic representation.
+        """
+        self.decoded_individuals = self.pop.decode_individuals(self.pop.Individuals)
+        # The following line is a placeholder for further decoding operations
+        # for i in range(self.population_size):
+        #     model = Net(pop.individual[0], self.in_channels, self.intermediate_channels, self.num_classes, self.blocks_size)
+
+    def optimize(self):
+        """
+        Run the optimization process, including evolving the population and evaluating individuals.
+        """
+        self.evolve()  # Call the evolve method to apply evolutionary operations like mutation, crossover, etc.
 
